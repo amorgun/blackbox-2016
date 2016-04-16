597197b902f579a9dc971a7ee4215838d0b53b94
amorgun <alexander.morgun.usu@gmail.com>
2016-04-17 00:09:50 +0600

    Nice oop interface

--- my_bot/bot.py
+++ my_bot/bot.py
@@ -1,62 +1,56 @@
 import interface as bbox
+from sklearn.linear_model import SGDRegressor
 import numpy as np
 
 
-def get_action_by_state(state):
-	best_act = -1
-	best_val = -1e9
- 
-	for act in range(n_actions):
-		val = calc_reg_for_action(act, state)
-		if val > best_val:
-			best_val = val
-			best_act = act
- 
-	return best_act
+class Predictor:
+    def __init__(self):
+        self.n_features = 36
+        self.n_actions = 4
+        self.max_time = -1
+    
+        if bbox.is_level_loaded():
+            bbox.reset_level()
+        else:
+            bbox.load_level("../levels/train_level.data", verbose=1)
+            self.n_features = bbox.get_num_of_features()
+            self.n_actions = bbox.get_num_of_actions()
+            self.max_time = bbox.get_max_time()
 
 
- 
-n_features = 36
-n_actions = 4
-max_time = -1
+    def load(self, filename):
+        coefs = np.loadtxt(filename).reshape(self.n_actions, self.n_features + 1)
+        self.reg_coefs = coefs[:,:-1]
+        self.free_coefs = coefs[:,-1]
+        
+    def calc_reg_for_action(self, action, state):
+        return np.dot(self.reg_coefs[action], state) + self.free_coefs[action]
+    
+    def get_action_by_state(self, state):
+        best_act = -1
+        best_val = -1e9
+     
+        for act in range(self.n_actions):
+            val = self.calc_reg_for_action(act, state)
+            if val > best_val:
+                best_val = val
+                best_act = act
+     
+        return best_act
+     
+    def run(self):
+        has_next = True
+        self.load("reg_coefs.txt")
+        while has_next:
+            state = bbox.get_state()
+            action = self.get_action_by_state(state)
+            has_next = bbox.do_action(action)
+     
+        bbox.finish(verbose=1)
 
- 
-def prepare_bbox():
-	global n_features, n_actions, max_time
- 
-	if bbox.is_level_loaded():
-		bbox.reset_level()
-	else:
-		bbox.load_level("../levels/train_level.data", verbose=1)
-		n_features = bbox.get_num_of_features()
-		n_actions = bbox.get_num_of_actions()
-		max_time = bbox.get_max_time()
- 
- 
-def load_regression_coefs(filename):
-	global reg_coefs, free_coefs
-	coefs = np.loadtxt(filename).reshape(n_actions, n_features + 1)
-	reg_coefs = coefs[:,:-1]
-	free_coefs = coefs[:,-1]
- 
- 
-def calc_reg_for_action(action, state):
-	return np.dot(reg_coefs[action], state) + free_coefs[action]
- 
- 
-def run_bbox():
-	has_next = 1
-	
-	prepare_bbox()
-	load_regression_coefs("reg_coefs.txt")
- 
-	while has_next:
-		state = bbox.get_state()
-		action = get_action_by_state(state)
-		has_next = bbox.do_action(action)
- 
-	bbox.finish(verbose=1)
+
  
  
 if __name__ == "__main__":
-	run_bbox()
+    predictor = Predictor()
+    predictor.run()
